bf580a6f6da97d9afa3d2b1ae9cc241a7a496fac
Krunali Jadhav <102589709+KrunaliJ@users.noreply.github.com>
2025-05-27 22:54:36 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,4 +1,5 @@
 //outer and inner class
+import java.util.Scanner;
 
 class OuterClass {
   int x = 10;
@@ -10,6 +11,17 @@ class OuterClass {
 
 public class Demo {
   public static void main(String[] args) {
+    int a,b,sum;
+    Scanner myObj = new Scanner(System.in); // Create a Scanner object
+    System.out.println("Type a number:");
+    x = myObj.nextInt(); // Read user input
+
+    System.out.println("Type another number:");
+    y = myObj.nextInt(); // Read user input
+
+    sum = x + y;  // Calculate the sum of x + y
+    System.out.println("Sum is: " + sum); // Print the sum
+    
     OuterClass myOuter = new OuterClass();
     OuterClass.InnerClass myInner = myOuter.new InnerClass();
     System.out.println(myInner.y + myOuter.x);
